b3393ab65bf58b3fdca7ac36c6f64244e1c46556
Srourrzan <razanmsrour@gmail.com>
2025-11-27 15:53:10 +0300

    refactor: update dependencies and enhance CSI collection in main.rs
    - Updated esp-hal to version 1.0.0-rc.0 and added esp-hal-embassy for better integration.
    - Changed esp-alloc version to 0.8.0 and updated embassy-executor and embassy-time versions.
    - Introduced esp-wifi for WiFi management with new features for CSI collection.
    - Refactored main function to initialize WiFi controller and CSI sniffer.
    - Implemented a loop for continuous CSI data collection with a timeout mechanism.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,15 +7,12 @@
 )]
 
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Timer, with_timeout};
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::timg::TimerGroup;
-use esp_radio::ble::controller::BleConnector;
-
-#[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
+use esp_hal::rng::Rng;
+use esp_csi_rs::{collector::CSISniffer, config::CSIConfig};
+use esp_wifi::{init, EspWifiController};
 
 extern crate alloc;
 
@@ -23,34 +20,63 @@ extern crate alloc;
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[esp_rtos::main]
-async fn main(spawner: Spawner) -> ! {
-    // generator version: 1.0.1
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
 
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    // Configure System Clock
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    // Take Peripherals
     let peripherals = esp_hal::init(config);
 
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66416);
-    // COEX needs more RAM - so we've added some more
-    esp_alloc::heap_allocator!(size: 64 * 1024);
+    // Allocate some heap space
+    esp_alloc::heap_allocator!(size: 72 * 1024);
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let sw_interrupt =
-        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+    // Initialize Embassy - ESP32-C2 only has TIMG0, so use systimer for embassy
+    let systimer = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER);
+    esp_hal_embassy::init(systimer.alarm0);
 
-    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
-    let (mut _wifi_controller, _interfaces) =
-        esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
-            .expect("Failed to initialize Wi-Fi controller");
-    let _connector = BleConnector::new(&radio_init, peripherals.BT, Default::default());
+    // Instantiate peripherals necessary to set up WiFi
+    let timer1 = TimerGroup::new(peripherals.TIMG0);
+    let wifi = peripherals.WIFI;
+    let timer = timer1.timer0;
+    let rng = Rng::new(peripherals.RNG);
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    // Initialize WiFi Controller
+    let init = &*mk_static!(EspWifiController<'static>, init(timer, rng).unwrap());
+    // Instantiate WiFi controller and interfaces
+    let (controller, interfaces) = esp_wifi::wifi::new(&init, wifi).unwrap();
 
+    // Create a Collector Instance
+    let mut csi_coll_snif = CSISniffer::new(CSIConfig::default(), controller).await;
+
+    // Initialize CSI Collector
+    csi_coll_snif.init(interfaces, &spawner).await.unwrap();
+
+    // Start Collection
+    csi_coll_snif.start_collection().await;
+
+    // Collect for 2 Seconds
+    with_timeout(Duration::from_secs(2), async {
+        loop {
+            csi_coll_snif.print_csi_w_metadata().await;
+        }
+    })
+    .await
+    .unwrap_err();
+
+    // Stop Collection
+    csi_coll_snif.stop_collection().await;
+
+    // Keep running
     loop {
         Timer::after(Duration::from_secs(1)).await;
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
 }
